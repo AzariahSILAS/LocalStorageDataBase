fc8173075d760617ab6bf91f21f0d3780cba4713
AzariahSILAS <95987530+AzariahSILAS@users.noreply.github.com>
2024-08-31 06:50:51 -0700

    I added a completely new feature and deletetd pretty much all the old logic im starting to change the way it thinks a little bit.

--- script.js
+++ script.js
@@ -1,56 +1,42 @@
 const submitebtn = document.getElementById('submitBtn');
-const unInput = document.getElementById('unInput');
-const pwInput = document.getElementById('pwInput');
+const unInput = document.getElementById('usernameInput');
+const pwInput = document.getElementById('passwordInput');
 const userNameRow = document.getElementById('userNameRow');
 const passwordRow = document.getElementById('passwordRow');
-const demo = document.getElementById('demo');
-
-
-
-function buildDataBase() {
-    const createUN = document.createElement('td');
-    const createPW = document.createElement('td');
-
-    
-    let newNum = Number(localStorage.getItem('number')); 
-     let j = newNum;
-     j++
-     localStorage.setItem(`number`, j )
- 
-    localStorage.setItem(`${j}username`, unInput.value)
-    localStorage.setItem(`${j}password`, pwInput.value)
-
-    window.location.reload()
- 
-
-}
+const table = document.getElementById('table')
 
 
 
 
-function buildForm(){
-
-    for(let i = 0; i < localStorage.length; i++) {
-        let j = localStorage.key(i)
-        let localValue = localStorage.getItem(j)
-        const createTableData = document.createElement('td')
-        createTableData.textContent = localValue
 
 
-        console.log(localValue)
-        if(localStorage.key(i).includes('password')){
-            passwordRow.appendChild(createTableData);
-        } else if(localStorage.key(i).includes('username')){
-            userNameRow.appendChild(createTableData)
-        }
 
+// submitebtn.addEventListener('click', buildDataBase)
+
+function buildRow() {
+    arr = []
+    for(i=0;i<2;i++){
+        const newRow = document.createElement('tr'); 
+        const newtd = document.createElement('th'); 
+        arr.push(newRow)
+        arr.push(newtd)
     }
+    const firstrow = arr[0]
+    const username = arr[1]
+    const secondrow = arr[2]
+    const password = arr[3]
+    username.textContent = 'username'
+    password.textContent = 'password'
+    table.appendChild(firstrow)
+    firstrow.appendChild(username)
+    table.appendChild(secondrow)
+    secondrow.appendChild(password)
 
+    console.log(arr)
+    
+    
 }
-buildForm()
-
-
-submitebtn.addEventListener('click', buildDataBase)
+buildRow()
 
 
 
